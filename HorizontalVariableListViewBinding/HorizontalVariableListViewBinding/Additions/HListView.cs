7d5ab1bd86c80f95f40353580a3fd231254f8f47
magicgoose <magic-goose@yandex.ru>
2014-07-22 01:17:36 +0400

    rename namespace in java project, remove some pieces

---
+++ HorizontalVariableListViewBinding/HorizontalVariableListViewBinding/Additions/HListView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IT.Sephiroth.Sandroid.Library.Widget {
+
+	public partial class HListView
+	{
+		protected override Java.Lang.Object RawAdapter {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+	}
+}
